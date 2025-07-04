e973c0e70ee065e6645bd2bd36d6de19c9e11e99
orifjonovhadyatiilo <orifjonovizzatillo007@gmail.com>
2025-07-04 14:31:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    for user in update.message.new_chat_members:
+        await update.message.reply_text(f"👋 Salom, {user.full_name}! Guruhimizga xush kelibsiz!")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
+
+if name == "main":
+    app.run_polling()
